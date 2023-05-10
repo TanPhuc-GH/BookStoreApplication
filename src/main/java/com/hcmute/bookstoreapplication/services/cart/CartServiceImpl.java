5cac7fe5a53170b7effa0194e740c6b0ac1673aa
killerburn69 <kietthetran123@gmail.com>
2023-05-10 19:55:59 +0700

    handle item cart

--- src/main/java/com/hcmute/bookstoreapplication/services/cart/CartServiceImpl.java
+++ src/main/java/com/hcmute/bookstoreapplication/services/cart/CartServiceImpl.java
@@ -10,6 +10,7 @@ import com.hcmute.bookstoreapplication.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,24 +24,58 @@ public class CartServiceImpl implements CartService{
     @Override
     public ItemDTO createItem(ItemRequestDTO itemRequestDTO) {
         Optional<Product> product = productRepository.findById(itemRequestDTO.getId());
+//        Optional<Item> optionalItem = itemRepository.findById(itemRequestDTO.getId());
         if(!product.isPresent()){
             throw new NotFoundException(String.format("Product with id %d not found.", itemRequestDTO.getId()));
         }
         Item item = new Item();
+        List<Item> items = itemRepository.findAll();
         ItemDTO itemDTO = new ItemDTO();
+//        Product productUpdate = new Product();
         Integer quantityProduct = product.get().getQuantity();
+        Integer count = 0;
         if(itemRequestDTO.getQuantity() <= quantityProduct) {
-            item.setProduct(product.get());
-            item.setItemName(product.get().getProductName());
-            item.setPrice(product.get().getPrice());
-            item.setQuantity(itemRequestDTO.getQuantity());
-            itemRepository.save(item);
-            itemDTO.setProduct_id(product.get().getId());
-            itemDTO.setItemName(product.get().getProductName());
-            itemDTO.setPrice(product.get().getPrice());
-            itemDTO.setQuantity(itemRequestDTO.getQuantity());
-            return itemDTO;
-
+            if(items.size()==0){
+                item.setProduct(product.get());
+                item.setItemName(product.get().getProductName());
+                item.setPrice(product.get().getPrice());
+                item.setQuantity(itemRequestDTO.getQuantity());
+                itemRepository.save(item);
+                product.get().setQuantity(quantityProduct - itemRequestDTO.getQuantity());
+                productRepository.save(product.get());
+                itemDTO.setProduct_id(product.get().getId());
+                itemDTO.setItemName(product.get().getProductName());
+                itemDTO.setPrice(product.get().getPrice());
+                itemDTO.setQuantity(itemRequestDTO.getQuantity());
+                return itemDTO;
+            }
+            else{
+                for(Item itemCart:items){
+                    if(itemCart.getProduct().getId() == itemRequestDTO.getId()){
+                        itemCart.setQuantity(itemRequestDTO.getQuantity() + itemCart.getQuantity());
+                        itemRepository.save(itemCart);
+                        product.get().setQuantity(quantityProduct - itemRequestDTO.getQuantity());
+                        productRepository.save(product.get());
+                        break;
+                    }
+                    count++;
+                }
+                if(count == items.size()){
+                    item.setProduct(product.get());
+                    item.setItemName(product.get().getProductName());
+                    item.setPrice(product.get().getPrice());
+                    item.setQuantity(itemRequestDTO.getQuantity());
+                    itemRepository.save(item);
+                    product.get().setQuantity(quantityProduct - itemRequestDTO.getQuantity());
+                    productRepository.save(product.get());
+                    itemDTO.setProduct_id(product.get().getId());
+                    itemDTO.setItemName(product.get().getProductName());
+                    itemDTO.setPrice(product.get().getPrice());
+                    itemDTO.setQuantity(itemRequestDTO.getQuantity());
+                    return itemDTO;
+                }
+                return itemDTO;
+            }
         }else {
             throw new RuntimeException("Quantity of product is larger than in warehouse");
         }
